92475b5e2425ad7545d79a807165fd9361c9e051
Connor Smiley <moxiesmiley@gmail.com>
2022-07-12 13:06:41 +0900

    v1

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,24 @@
 import { NextPage } from "next";
-import Counter from "@/pages/Counter";
-import Posts from "@/pages/Posts";
+import styled from "styled-components";
+import tw from "twin.macro"
+
+
+const HomeContainer = styled.div`
+    ${tw`
+    bg-black
+    w-screen
+    h-screen
+    
+    `}
+`
 
 
 const IndexPage: NextPage = () => {
   return (
     <>
-      <Posts />
+      <HomeContainer>
+
+      </HomeContainer>
     </>
   );
 };
--- src/redux/store.tsx
+++ src/redux/store.tsx
@@ -1,10 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "../redux/features/counterSlice"
-import postsReducer from "../redux/features/postsSlice"
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
-    posts: postsReducer
   }
 })
