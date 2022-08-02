25cd7801975022aa7a2c4b6467fd76279dcab981
Connor Smiley <moxiesmiley@gmail.com>
2022-08-02 23:52:07 +0900

    slight-progress

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import styled from "styled-components";
+import tw from "twin.macro";
+
+const SidebarContainer = styled.div`
+  ${tw`
+    
+    `}
+`;
+
+const SidebarStyle = styled.div`
+  ${tw`
+    
+    `}
+`;
+
+export interface ISidebarProps {
+
+}
+
+const Sidebar: React.FunctionComponent<ISidebarProps> = () => {
+
+  return (
+    <>
+      <SidebarContainer>
+        <SidebarStyle>
+
+
+
+        </SidebarStyle>
+      </SidebarContainer>
+    </>
+  );
+};
+
+export default Sidebar;
+
+
+
